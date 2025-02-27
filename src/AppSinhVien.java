4c82e3804aeea64ee98611fe4901d4530630865d
CGM <mrmouse.guru@gmail.com>
2025-02-27 17:23:43 +0700

    bài tập sinh viên - Sinh Vien 3 - nhâp thông tin từ bàn phím

--- src/AppSinhVien.java
+++ src/AppSinhVien.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class AppSinhVien {
 
@@ -11,10 +12,22 @@ public class AppSinhVien {
 		SinhVien sv2;
 		SinhVien sv3;
 		float diemTB;
+		Scanner keyboardInput;
+		int maSV;
 		
 			      //bước 2 
 	//bước3	     //khởi tạo đối tượng
 		sv1 =    new SinhVien(1111, "Le Van teo", 2, 2);
+		//sv2
+		//nhập từ ban phím thông tin
+		//sv3
+		//bước 3         //bước 2
+	   keyboardInput =	new Scanner(System.in);
+	   System.out.print("[NHAP MA SV]:");
+	   maSV = keyboardInput.nextInt();
+	   sv3 = new SinhVien();
+	   sv3.maSV = maSV;// thay đổi trạng thái của object
+		
 		
 		//gửi thông điệp đến hành vi tinhDiemTrungBinh()
 		diemTB = sv1.tinhDiemTrungBinh();
@@ -22,6 +35,9 @@ public class AppSinhVien {
 		//kết quả
 		System.out.println("diem trung binh cua sv1: " + diemTB);
 		System.out.println("ten sv1: " + sv1.hoTen);
+		
+		//in sinh viên3
+		System.out.println("ma sinh vien 3: " + sv3.maSV);
 	}
 
 }
